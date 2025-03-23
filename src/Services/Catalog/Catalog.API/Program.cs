107cb1a8010164fd75f4f138b1f6c4c59c4b9ac5
Seif Seoud <114704120+SeifSeoud@users.noreply.github.com>
2025-03-24 01:48:39 +0200

    add CreateProduct

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add Service to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
-
+// Configure the HTTP request pipelines
+app.MapCarter();
 app.Run();
